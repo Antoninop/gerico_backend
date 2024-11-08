6f4b68cb3d5c9688d8fa522fc5f72d6e248683a2
Antonino <antoninopiraino70@gmail.com>
2024-11-08 13:11:32 +0100

    [+] swagger integration

--- app.js
+++ app.js
@@ -2,16 +2,21 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs'); 
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 
 dotenv.config();
 
 const app = express();
 const port = 3000;
 
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.use(cors());
 app.use(bodyParser.json());
 
-//  log des requêtes
 app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   console.log('Headers:', req.headers);

3f50c1156b3814c3ba96cd64b0606b72c695fa33
Antonino <antoninopiraino70@gmail.com>
2024-11-11 01:57:11 +0100

    + fetchAccountInfo call

--- src/account.js
+++ src/account.js
@@ -2,7 +2,7 @@ const db = require('./db');
 const { promisify } = require('util');
 const query = promisify(db.query).bind(db);
 
-exports.fetchHolidayInfo = async (req, res) => {
+exports.fetchAccountInfo = async (req, res) => {
   const id_user = req.user?.id;
 
   if (!id_user) {
@@ -10,16 +10,16 @@ exports.fetchHolidayInfo = async (req, res) => {
   }
 
   try {
-    const sql = 'SELECT remaining_holidays FROM Users WHERE id = ?';
+    const sql = 'SELECT first_name, last_name, email, hire_date, position, salary ,is_admin FROM Users WHERE id = ?';
     const results = await query(sql, [id_user]);
 
     if (!results || results.length === 0) {
-      return res.status(404).json({ message: 'No holiday information found for this user.' });
+      return res.status(404).json({ message: 'No account information found for this user.' });
     }
 
-    res.status(200).json({ remaining_holidays: results[0].remaining_holidays });
+    res.status(200).json(results[0]);
   } catch (err) {
-    console.error('Error fetching holiday information:', err);
+    console.error(`Error fetching account information for user ${id_user}:`, err);
     return res.status(500).json({ message: 'Internal server error' });
   }
 };
--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const { loginUser, createUser, fetchPayroll } = require('./userController');
 const { fetchHolidayInfo } = require('./holiday');
+const { fetchAccountInfo } = require('./account');
 const { authenticateJWT } = require('./authMiddleware');
 const { benchmark } = require('./benchmark/benchmark');
 
@@ -11,8 +12,8 @@ router.post('/login', loginUser);
 router.post('/register', createUser); 
 
 router.get('/fetchPayroll', authenticateJWT, fetchPayroll); 
-
 router.get('/fetchHolidayInfo', authenticateJWT, fetchHolidayInfo); 
+router.get('/fetchAccountInfo', authenticateJWT, fetchAccountInfo); 
 
 
 router.post('/benchmark', benchmark);

492c9d44ec6b217128222a537f8bdd78fcdb5a36
Antonino <antoninopiraino70@gmail.com>
2024-11-25 13:00:41 +0100

    + email sender

--- src/mail.js
+++ src/mail.js
@@ -3,6 +3,7 @@ const { promisify } = require('util');
 const db = require('./db');
 const query = promisify(db.query).bind(db);
 const { generateSmallINT } = require('./utils');
+const codeMemory = new Map();
 
 const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST || 'smtp.gmail.com',
@@ -14,7 +15,7 @@ const transporter = nodemailer.createTransport({
   },
 });
 
-exports.sendHelloWorld = async (req, res) => {
+exports.sendCodeResetPass = async (req, res) => {
   const { email } = req.body;
 
   if (!email) {
@@ -29,20 +30,45 @@ exports.sendHelloWorld = async (req, res) => {
   try {
     const sql = 'SELECT email FROM Users WHERE email = ?';
     const results = await query(sql, [email]);
+    const code = generateSmallINT();
 
     const mailOptions = {
       from: process.env.SMTP_USER,
       to: email,
       subject: 'Code de récupèration de mot de passe',
-      text: 'Voici votre code : ' + generateSmallINT(),
+      text: 'Voici votre code : ' + code,
     };
 
     if (results.length === 1) {
       await transporter.sendMail(mailOptions);
+      codeMemory.set(email, code);
     }
 
   } catch (error) {
     console.error('Error during the process:', error);
     return res.status(500).json({ message: 'Internal server error', error });
+  }
+};
+
+
+exports.verifyCodeResetPass = async (req, res) => {
+  const { email, code } = req.body;
+
+  if (!email || !code) {
+    return res.status(400).json({ message: 'Adresse email et code requis' });
+  }
+
+  const storedCode = codeMemory.get(email);
+
+  if (!storedCode) {
+    return res.status(404).json({ message: 'Code non trouvé ou expiré' });
+  }
+
+  if (parseInt(code.trim(), 10) === parseInt(storedCode.trim(), 10)) {
+    codeMemory.delete(email); 
+    return res.status(200).json({ message: 'Code valide' });
+  } else {
+    console.log('Mismatch:', code.trim(), storedCode.trim());
+    return res.status(400).json({ message: 'Code incorrect' });
   }
 };
--- src/routes.js
+++ src/routes.js
@@ -5,7 +5,7 @@ const { fetchAccountInfo } = require('./account');
 const { fetchArchivedUsers , fetchUsers ,fetchAskedHoliday } = require ('./admin');
 const { authenticateJWT } = require('./authMiddleware');
 const { benchmark } = require('./benchmark/benchmark');
-const { sendHelloWorld } = require('./mail')
+const { sendCodeResetPass , verifyCodeResetPass } = require('./mail')
 
 
 const router = express.Router();
@@ -21,6 +21,6 @@ router.get('/fetchUsers', authenticateJWT, fetchUsers);
 router.post('/askHoliday' , authenticateJWT, askHoliday);
 router.post('/benchmark', benchmark);
 
-router.post('/send-hello', sendHelloWorld);
-
+router.post('/sendCodeResetPass', sendCodeResetPass);
+router.post('/verifyCodeResetPass', verifyCodeResetPass)
 module.exports = router;
